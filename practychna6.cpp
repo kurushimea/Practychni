e1cb7daae78e226d464123d470ea5056977af5d6
kurushimea <160355798+kurushimea@users.noreply.github.com>
2025-07-07 20:27:57 +0300

    Add files via upload

---
+++ practychna6.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int x1, y1, x2, y2;
+    scanf_s("%d %d %d %d", &x1, &y1, &x2, &y2);
+
+    double length = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+    printf("%.6f\n", length);
+    return 0;
+}
