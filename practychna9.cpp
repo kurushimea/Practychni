37a55c91780b333eb474b8cf4193ca6968a570a4
kurushimea <160355798+kurushimea@users.noreply.github.com>
2025-07-07 20:30:15 +0300

    Add files via upload

---
+++ practychna9.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int min_steps(int x, int y) {
+    long long dist = y - x;
+    long long step = 0;
+    long long sum = 0;
+
+    while (sum < dist) {
+        step++;
+        sum += step * 2; 
+    }
+
+
+    if (sum - step >= dist)
+        return step * 2 - 1;
+    else
+        return step * 2;
+}
+
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    printf("%d\n", min_steps(x, y));
+    return 0;
+}
